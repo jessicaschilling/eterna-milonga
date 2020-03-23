1dd79bb1e80b6f02bfab52b28a77df584e9c0b34
Iris Berkeley <irisberkeley@Iris-Mac.local>
2020-03-22 20:23:35 -0600

    note: must be logged in to Spotify

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ function App() {
       <p>
         Reload this page to get another tanda.
         <br/>
+        <strong>Note!</strong> You must be logged in to Spotify for full songs to play.
+        <br/>
         <small>Made with &#128155; by <a href="https://www.jessicaschilling.com" target="_blank" rel="noopener noreferrer">Jessica Schilling</a> | <a href="https://github.com/jessicaschilling/eterna-milonga" target="_blank" rel="noopener noreferrer">GitHub</a></small>
       </p>
     </div>
